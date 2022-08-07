61d0bd9b873f1ee1a6b623eb58e2d8551800d582
Greentima <blinovtimofey@gmail.com>
2022-08-07 12:21:09 +0300

    3 project

--- Pr01/Program.cs
+++ Pr01/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("3 Hello, World!");
+Console.WriteLine("Введите ваше имя:");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
